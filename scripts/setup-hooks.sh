6a24095ffb1b3ea8d8499fe7d237ad88689441f4
Florian Geißer <geisserf@informatik.uni-freiburg.de>
2017-06-27 14:39:03 +0200

    Changed hook setup to pre-commit filename.

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -2,5 +2,5 @@
 root="`git rev-parse --show-toplevel`"
 dir="$root/.git"
 
-cp $root/scripts/pre-commit-clang-format $dir/hooks
-echo "hook script set up in $dir/hooks" 
+cp $root/scripts/pre-commit-clang-format $dir/hooks/pre-commit
+echo "pre-commit hook script set up in $dir/hooks" 
