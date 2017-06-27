fff7e4053a1e159854e4bfb1896ed64fb7103ec7
Benedict Wright <bwright@informatik.uni-freiburg.de>
2017-06-27 13:59:00 +0200

    added basic bechmark

---
+++ run_benchmark.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+./build.py --debug -j4
+
+./builds/debug32/bin/Benchmark
+pprof --callgrind builds/debug32/bin/Benchmark MyProfileFilename >profile.callgrind
+kcachegrind profile.callgrind
+
+#pprof builds/debug32/bin/Benchmark /tmp/prof.out
+#pprof --gv  builds/debug32/bin/Benchmark /tmp/prof.out
---
+++ src/benchmark/benchmark.cpp
@@ -0,0 +1,31 @@
+#include <gperftools/profiler.h>
+#include "../Polynomial.h"
+#include "../catamorph/Factories.h"
+#include <unistd.h>
+
+int main() {
+  ProfilerStart("MyProfileFilename");
+
+  Expression e = Factories::add(
+      {Factories::cst(1), Factories::cst(2),
+       Factories::mul({Factories::cst(0), Factories::var((ID) "x"),
+                       Factories::var((ID) "y")}),
+       Factories::mul(
+           {Factories::cst(1), Factories::var((ID) "y"), Factories::cst(2)}),
+       Factories::add({Factories::cst(0), Factories::var((ID) "x")})});
+
+
+
+  Env env = {{"x", 0},{"y",0}};
+  for(int i = 0;i<1000;i++){
+    for(int j=0;j<1000;j++){
+      Polynomial p = Polynomial(e);
+      env = {{"x", i},{"y",j}};
+      p.evaluate(env);
+      p.print();
+    }
+  }
+
+  ProfilerStop();
+  return 0;
+}
