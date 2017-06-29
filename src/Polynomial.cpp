19c3964726c77897867438efd41463b93b9bc4a7
Florian Geißer <geisserf@informatik.uni-freiburg.de>
2017-06-29 16:42:11 +0200

    Refactored polynomial class.

--- src/Polynomial.cpp
+++ src/Polynomial.cpp
@@ -1,63 +1,42 @@
 #include "Polynomial.h"
-#include "catamorph/Expression.h"
+#include "parser.h"
+
 #include "catamorph/Printer.h"
 #include "catamorph/interpreters/Dependencies.h"
 #include "catamorph/interpreters/Evaluate.h"
-#include "parser.h"
+
 #include <string>
 
-Polynomial::Polynomial(std::string expression) {
-    this->s_expression = expression;
-    this->parseExpressionString();
-}
-
-Polynomial::Polynomial(const Polynomial &e) {
-    this->expression = e.expression;
-}
-
-Polynomial::Polynomial(Expression e) {
-    this->expression = std::make_shared<Expression>(e);
-}
-
-void Polynomial::evaluate(const Env &environment) {
-    // We can do better than this casting -> requires proper memory management
-    this->expression = std::make_shared<Expression>(
-        Evaluate::partial_eval(environment, *(this->expression.get())));
-}
-
-NBR Polynomial::getAndSubtractConst() {
-    std::cout << "\033[1;33m WARNING! no environment provided! try provding "
-                 "environment All = 0 for better performance \033[0m"
-              << '\n';
-
-    std::set<ID> dependencies =
-        Dependency::dependencies(*(this->expression.get()));
-    Env environment = Env();
-    for (auto f : dependencies) {
-        environment.insert(std::pair<ID, NBR>(f, 0));
-    }
-    return this->getAndSubtractConst(environment);
-}
-
-NBR Polynomial::getAndSubtractConst(const Env &environment) {
-    Expression res =
-        Evaluate::partial_eval(environment, *(this->expression.get()));
-
-    this->expression = std::make_shared<Expression>(
-        Factories::sub({*(this->expression.get()), res.get()}));
-    return *(Factories::get_as_cst(res.get()));
-}
-
-void Polynomial::print() {
-    Printer::print(*(this->expression.get()));
-}
-
-std::string Polynomial::toString() {
-    return Printer::asString(*(this->expression.get()));
-}
-
-void Polynomial::parseExpressionString() {
+Polynomial::Polynomial(std::string const &input) {
     InfixParser parser;
-    this->expression =
-        std::make_shared<Expression>(parser.parse(this->s_expression));
+    expression = parser.parse(input);
+}
+
+Polynomial::Polynomial(Expression const &e) {
+    expression = e;
+}
+
+Expression Polynomial::evaluate(const Env &environment) const {
+    return Evaluate::partial_eval(environment, expression);
+}
+
+Expression Polynomial::getConstantPart() const {
+    std::set<ID> dependencies = Dependency::dependencies(expression);
+    Env environment = Env();
+    for (auto const &var : dependencies) {
+        environment.insert(std::pair<ID, NBR>(var, 0));
+    }
+    return Evaluate::partial_eval(environment, expression);
+}
+
+Expression Polynomial::subtractConstant(Expression const &constant) const {
+    return Factories::sub({expression, constant});
+}
+
+void Polynomial::print() const {
+    Printer::print(expression);
+}
+
+std::string Polynomial::toString() const {
+    return Printer::asString(expression);
 }
