56880a230535e2579e6dd8e071560816325e7319
Florian Geißer <geisserf@informatik.uni-freiburg.de>
2017-09-29 16:19:08 +0200

    Iversion evmdd tests now compile, but required operators are not yet implemented.

--- src/test/iverson_evmdd_tests.cpp
+++ src/test/iverson_evmdd_tests.cpp
@@ -1,6 +1,5 @@
 #include "../catamorph/factories.h"
 #include "../catamorph/interpreters/create_evmdd.h"
-#include "../evmdd/evmdd_expression.h"
 #include "../polynomial.h"
 #include "Catch/include/catch.hpp"
 #include <iostream>
@@ -10,51 +9,41 @@ SCENARIO("Testing iverson EVMDD construction", "[iversonevmdd]") {
         Expression e = Factories::land(
             {Factories::var((ID) "a"), Factories::var((ID) "b")});
 
-        WHEN("Create EVMDD") {
-            Domains d = {{"a", 2}, {"b", 2}};
-            Ordering o = {{"a", 1}, {"b", 2}};
-            CreateEvmdd<NumericExpression> create;
-            Evmdd<NumericExpression> evmdd = create.create_evmdd(e, d, o);
-            // evmdd.print(std::cout);
+        Domains d = {{"a", 2}, {"b", 2}};
+        Ordering o = {{"a", 1}, {"b", 2}};
+        CreateEvmdd<float> create;
+        auto evmdd = create.create_evmdd(e, d, o);
 
+        ConcreteState state(2);
+        WHEN("We evaluate the corresponding EVMDD for all domain values") {
             for (int a = 0; a <= 1; ++a) {
                 for (int b = 0; b <= 1; ++b) {
-                    std::vector<int> values_a{a};
-                    std::vector<int> values_b{b};
-                    std::map<std::string, std::vector<int>> state{
-                        {"a", values_a}, {"b", values_b}};
-                    auto result = evmdd.evaluate_partial(state);
-                    THEN("result shoud be a & b") {
-                        REQUIRE(result[0].value == (a && b));
+                    state[0] = a;
+                    state[1] = b;
+                    THEN("The result should be a & b") {
+                        REQUIRE(evmdd.evaluate(state) == (a && b));
                     }
                 }
             }
         }
     }
 
-    GIVEN("The expression (a & b) *10") {
+    GIVEN("The expression (a & b) * 10") {
         Expression e =
             Factories::mul({Factories::land({Factories::var((ID) "a"),
                                              Factories::var((ID) "b")}),
                             Factories::cst(10)});
-        WHEN("Create EVMDD") {
+        WHEN("We create the EVMDD") {
             Domains d = {{"a", 2}, {"b", 2}};
             Ordering o = {{"a", 1}, {"b", 2}};
-            CreateEvmdd<NumericExpression> create;
-            Evmdd<NumericExpression> evmdd = create.create_evmdd(e, d, o);
-            // evmdd.print(std::cout);
-            THEN("Evaluation with a = 1 & B=1 should be 10") {
-                std::vector<int> values_a{1};
-                std::vector<int> values_b{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a},
-                                                              {"b", values_b}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 10);
+            CreateEvmdd<float> create;
+            auto evmdd = create.create_evmdd(e, d, o);
+            THEN("Evaluation with a = 1 & b = 1 should be 10") {
+                ConcreteState state{1, 1};
+                REQUIRE(evmdd.evaluate(state) == 10);
             }
-            THEN("get min should be 0") {
-                std::map<std::string, std::vector<int>> state;
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 0);
+            THEN("the minimum should be 0") {
+                REQUIRE(evmdd.get_min() == 0);
             }
         }
     }
@@ -63,25 +52,18 @@ SCENARIO("Testing iverson EVMDD construction", "[iversonevmdd]") {
         Expression e = Factories::equals(
             {Factories::var((ID) "a"), Factories::var((ID) "b")});
 
-        WHEN("Create EVMDD") {
+        WHEN("We create the EVMDD") {
             Domains d = {{"a", 2}, {"b", 2}};
             Ordering o = {{"a", 1}, {"b", 2}};
-            CreateEvmdd<NumericExpression> create;
-            Evmdd<NumericExpression> evmdd = create.create_evmdd(e, d, o);
-            // evmdd.print(std::cout);
 
-            THEN("Evaluation with a = 1 & b=1 should be 1") {
-                std::vector<int> values_a{1};
-                std::vector<int> values_b{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a},
-                                                              {"b", values_b}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 1);
+            CreateEvmdd<double> create;
+            auto evmdd = create.create_evmdd(e, d, o);
+            THEN("Evaluation with a = 1 & b = 1 should be 1") {
+                ConcreteState state{1, 1};
+                REQUIRE(evmdd.evaluate(state) == 1);
             }
-            THEN("get min should be 0") {
-                std::map<std::string, std::vector<int>> state;
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 0);
+            THEN("the minimum should be 0") {
+                REQUIRE(evmdd.get_min() == 0);
             }
         }
     }
@@ -91,26 +73,18 @@ SCENARIO("Testing iverson EVMDD construction", "[iversonevmdd]") {
             Factories::mul({Factories::equals({Factories::var((ID) "a"),
                                                Factories::var((ID) "b")}),
                             Factories::cst(10)});
-
-        WHEN("Create EVMDD") {
+        WHEN("We create the EVMDD") {
             Domains d = {{"a", 2}, {"b", 2}};
             Ordering o = {{"a", 1}, {"b", 2}};
-            CreateEvmdd<NumericExpression> create;
-            Evmdd<NumericExpression> evmdd = create.create_evmdd(e, d, o);
-            // evmdd.print(std::cout);
+            CreateEvmdd<double> create;
+            auto evmdd = create.create_evmdd(e, d, o);
 
-            THEN("Evaluation with a = 1 & b=1 should be 10") {
-                std::vector<int> values_a{1};
-                std::vector<int> values_b{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a},
-                                                              {"b", values_b}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 10);
+            THEN("Evaluation with a = 1 & b = 1 should be 10") {
+                ConcreteState state{1, 1};
+                REQUIRE(evmdd.evaluate(state) == 10);
             }
-            THEN("get min should be 0") {
-                std::map<std::string, std::vector<int>> state;
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 0);
+            THEN("the minimum should be 0") {
+                REQUIRE(evmdd.get_min() == 0);
             }
         }
     }
@@ -119,25 +93,18 @@ SCENARIO("Testing iverson EVMDD construction", "[iversonevmdd]") {
         Expression e = Factories::lor(
             {Factories::var((ID) "a"), Factories::var((ID) "b")});
 
-        WHEN("Create EVMDD") {
+        WHEN("We create the EVMDD") {
             Domains d = {{"a", 2}, {"b", 2}};
             Ordering o = {{"a", 1}, {"b", 2}};
-            CreateEvmdd<NumericExpression> create;
-            Evmdd<NumericExpression> evmdd = create.create_evmdd(e, d, o);
-            // evmdd.print(std::cout);
+            CreateEvmdd<int> create;
+            auto evmdd = create.create_evmdd(e, d, o);
 
-            THEN("Evaluation with a = 1 & b=1 should be 1") {
-                std::vector<int> values_a{1};
-                std::vector<int> values_b{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a},
-                                                              {"b", values_b}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 1);
+            THEN("Evaluation with a = 1 & b = 1 should be 1") {
+                ConcreteState state{1, 1};
+                REQUIRE(evmdd.evaluate(state) == 1);
             }
-            THEN("get min should be 0") {
-                std::map<std::string, std::vector<int>> state;
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 0);
+            THEN("the minimum should be 0") {
+                REQUIRE(evmdd.get_min() == 0);
             }
         }
     }
@@ -147,88 +114,63 @@ SCENARIO("Testing iverson EVMDD construction", "[iversonevmdd]") {
             Factories::mul({Factories::lor({Factories::var((ID) "a"),
                                             Factories::var((ID) "b")}),
                             Factories::cst(10)});
-
-        WHEN("Create EVMDD") {
+        WHEN("We create the EVMDD") {
             Domains d = {{"a", 2}, {"b", 2}};
             Ordering o = {{"a", 1}, {"b", 2}};
-            CreateEvmdd<NumericExpression> create;
-            Evmdd<NumericExpression> evmdd = create.create_evmdd(e, d, o);
-            // evmdd.print(std::cout);
+            CreateEvmdd<int> create;
+            auto evmdd = create.create_evmdd(e, d, o);
 
-            THEN("Evaluation with a = 1 & b=1 should be 10") {
-                std::vector<int> values_a{1};
-                std::vector<int> values_b{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a},
-                                                              {"b", values_b}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 10);
+            THEN("Evaluation with a = 1 & b = 1 should be 10") {
+                ConcreteState state{1, 1};
+                REQUIRE(evmdd.evaluate(state) == 10);
             }
-            THEN("Evaluation with a = 0 & b=1 should be 10") {
-                std::vector<int> values_a{0};
-                std::vector<int> values_b{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a},
-                                                              {"b", values_b}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 10);
+            THEN("Evaluation with a = 0 & b = 1 should be 10") {
+                ConcreteState state{0, 1};
+                REQUIRE(evmdd.evaluate(state) == 10);
             }
-            THEN("Evaluation with a = 1 & b=0 should be 10") {
-                std::vector<int> values_a{1};
-                std::vector<int> values_b{0};
-                std::map<std::string, std::vector<int>> state{{"a", values_a},
-                                                              {"b", values_b}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 10);
+            THEN("Evaluation with a = 1 & b = 0 should be 10") {
+                ConcreteState state{1, 0};
+                REQUIRE(evmdd.evaluate(state) == 10);
             }
-            THEN("get min should be 0") {
-                std::map<std::string, std::vector<int>> state;
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 0);
+            THEN("the minimum should be 0") {
+                REQUIRE(evmdd.get_min() == 0);
             }
         }
     }
 
-    GIVEN("THE expression !(a)") {
+    GIVEN("The expression !(a)") {
         Expression e = Factories::lnot({Factories::var((ID) "a")});
-        WHEN("Create EVMDD") {
+        WHEN("We create the EVMDD") {
             Domains d = {{"a", 2}};
             Ordering o = {{"a", 1}};
-            CreateEvmdd<NumericExpression> create;
-            Evmdd<NumericExpression> evmdd = create.create_evmdd(e, d, o);
-            // evmdd.print(std::cout);
-            THEN("Evaluation a=1 should be 0") {
-                std::vector<int> values_a{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 0);
+            CreateEvmdd<float> create;
+            auto evmdd = create.create_evmdd(e, d, o);
+            THEN("Evaluation a = 1 should be 0") {
+                ConcreteState state{1};
+                REQUIRE(evmdd.evaluate(state) == 0);
             }
-            THEN("Evaluation a=0 should be 1") {
-                std::vector<int> values_a{0};
-                std::map<std::string, std::vector<int>> state{{"a", values_a}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 1);
+            THEN("Evaluation a = 0 should be 1") {
+                ConcreteState state{0};
+                REQUIRE(evmdd.evaluate(state) == 1);
             }
         }
     }
+
     GIVEN("THE expression !(a)*10") {
         Expression e = Factories::mul(
             {Factories::lnot({Factories::var((ID) "a")}), Factories::cst(10)});
         WHEN("Create EVMDD") {
             Domains d = {{"a", 2}};
             Ordering o = {{"a", 1}};
-            CreateEvmdd<NumericExpression> create;
-            Evmdd<NumericExpression> evmdd = create.create_evmdd(e, d, o);
-            // evmdd.print(std::cout);
-            THEN("Evaluation a=1 should be 0") {
-                std::vector<int> values_a{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 0);
+            CreateEvmdd<float> create;
+            auto evmdd = create.create_evmdd(e, d, o);
+            THEN("Evaluation a = 1 should be 0") {
+                ConcreteState state{1};
+                REQUIRE(evmdd.evaluate(state) == 0);
             }
-            THEN("Evaluation a=0 should be 10") {
-                std::vector<int> values_a{0};
-                std::map<std::string, std::vector<int>> state{{"a", values_a}};
-                auto result = evmdd.evaluate_partial(state);
-                REQUIRE(result[0].value == 10);
+            THEN("Evaluation a = 0 should be 10") {
+                ConcreteState state{0};
+                REQUIRE(evmdd.evaluate(state) == 10);
             }
         }
     }
