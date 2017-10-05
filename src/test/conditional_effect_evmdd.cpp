fbabd50d49a51e0c827b28ecfb0434ceaa6eb032
Florian Geißer <geisserf@informatik.uni-freiburg.de>
2017-10-05 17:06:19 +0200

    Refactored conditional effect evmdd tests.

--- src/test/conditional_effect_evmdd.cpp
+++ src/test/conditional_effect_evmdd.cpp
@@ -2,6 +2,7 @@
 #include "../conditional_effects.h"
 #include "../effect_parser.h"
 #include "../polynomial.h"
+
 #include "Catch/include/catch.hpp"
 #include <iostream>
 
@@ -10,20 +11,17 @@ SCENARIO("Testing basic conditional effect EVMDD creation", "[CE_evmdd]") {
         std::string e = "a";
         Polynomial p = Polynomial(e);
         ConditionalEffect effect = ConditionalEffect(p.getExpression(), "b", 1);
-        WHEN("Evaluating with s: a=1") {
+        ConditionalEffects effects = ConditionalEffects({effect});
+        WHEN("We create the EVMDD") {
             Domains d = {{"a", 2}, {"b", 2}};
             Ordering o = {{"a", 1}, {"b", 2}};
-            ConditionalEffects effects = ConditionalEffects({effect});
-            Evmdd<VariableAssignmentExpression> evmdd =
-                effects.create_evmdd(d, o);
-            THEN("No error should occur") {
-                std::vector<int> values_a{1};
-                std::map<std::string, std::vector<int>> state{{"a", values_a}};
-                auto result = evmdd.evaluate_partial(state);
-                THEN("result shoud be a & b") {
-                    REQUIRE(result[0].value.size() == 1);
-                    REQUIRE(result[0].value[0].toString() == "b=1");
-                }
+            Evmdd<Facts, Union> evmdd = effects.create_evmdd(d, o);
+            THEN("Evaluation with a = 1 & b = 0 should be {b=1}") {
+                ConcreteState state{1, 0};
+                Facts result = evmdd.evaluate(state);
+                REQUIRE(result.size() == 1);
+                REQUIRE(result.begin()->first == "b");
+                REQUIRE(result.begin()->second == 1);
             }
         }
     }
@@ -34,64 +32,38 @@ SCENARIO("Testing basic conditional effect EVMDD creation", "[CE_evmdd]") {
         ConditionalEffect effect = ConditionalEffect(e, "v", 0);
 
         Expression e2 = Factories::lnot({Factories::var((ID) "x")});
-        // Expression e2 = Factories::var((ID) "x");
 
         ConditionalEffect effect2 = ConditionalEffect(e2, "z", 1);
-        WHEN("get_min") {
-            Domains d = {{"x", 2}, {"y", 2}, {"v", 2}, {"z", 2}};
-            Ordering o = {{"x", 1}, {"y", 2}, {"v", 3}, {"z", 4}};
-
-            ConditionalEffects effects = ConditionalEffects({effect2, effect});
-            Evmdd<VariableAssignmentExpression> evmdd =
-                effects.create_evmdd(d, o);
-
-            // evmdd.print(std::cout);
-            auto result = evmdd.get_min();
-            THEN("result should be: ") {
-                REQUIRE(result[0].value.size() == 2);
-                REQUIRE(result[0].value[0].toString() == "v=0");
-                REQUIRE(result[0].value[1].toString() == "z=1");
+        ConditionalEffects effects = ConditionalEffects({effect2, effect});
+        WHEN("We create the EVMDD") {
+            Domains d = {{"x", 2}, {"y", 2}};
+            Ordering o = {{"x", 1}, {"y", 2}};
+            Evmdd<Facts, Union> evmdd = effects.create_evmdd(d, o);
+            THEN("Evaluation with x = 0 & y = 0 should be {z=1}") {
+                ConcreteState state{0, 0};
+                Facts result = evmdd.evaluate(state);
+                REQUIRE(result.size() == 1);
+                REQUIRE(result.begin()->first == "z");
+                REQUIRE(result.begin()->second == 1);
             }
-        }
-
-        WHEN("eavluate x = 1, y=1") {
-            Domains d = {{"x", 2}, {"y", 2}, {"v", 2}, {"z", 2}};
-            Ordering o = {{"x", 1}, {"y", 2}, {"v", 3}, {"z", 4}};
-
-            ConditionalEffects effects = ConditionalEffects({effect2, effect});
-            Evmdd<VariableAssignmentExpression> evmdd =
-                effects.create_evmdd(d, o);
-
-            std::vector<int> values_x{1};
-            std::vector<int> values_y{1};
-            std::map<std::string, std::vector<int>> state{{"x", values_x},
-                                                          {"y", values_y}};
-
-            // evmdd.print(std::cout);
-            auto result = evmdd.evaluate_partial(state);
-            THEN("result should be: ") {
-                REQUIRE(result[0].value.size() == 1);
-                REQUIRE(result[0].value[0].toString() == "v=0");
+            THEN("Evaluation with x = 0 & y = 1 should be {z=1}") {
+                ConcreteState state{0, 1};
+                Facts result = evmdd.evaluate(state);
+                REQUIRE(result.size() == 1);
+                REQUIRE(result.begin()->first == "z");
+                REQUIRE(result.begin()->second == 1);
             }
-        }
-        WHEN("eavluate x = 0, y=1") {
-            Domains d = {{"x", 2}, {"y", 2}, {"v", 2}, {"z", 2}};
-            Ordering o = {{"x", 1}, {"y", 2}, {"v", 3}, {"z", 4}};
-
-            ConditionalEffects effects = ConditionalEffects({effect2, effect});
-            Evmdd<VariableAssignmentExpression> evmdd =
-                effects.create_evmdd(d, o);
-
-            std::vector<int> values_x{0};
-            std::vector<int> values_y{1};
-            std::map<std::string, std::vector<int>> state{{"x", values_x},
-                                                          {"y", values_y}};
-
-            // evmdd.print(std::cout);
-            auto result = evmdd.evaluate_partial(state);
-            THEN("result should be: ") {
-                REQUIRE(result[0].value.size() == 1);
-                REQUIRE(result[0].value[0].toString() == "z=1");
+            THEN("Evaluation with x = 1 & y = 0 should be {}") {
+                ConcreteState state{1, 0};
+                Facts result = evmdd.evaluate(state);
+                REQUIRE(result.empty());
+            }
+            THEN("Evaluation with x = 1 & y = 1 should be {v=0}") {
+                ConcreteState state{1, 1};
+                Facts result = evmdd.evaluate(state);
+                REQUIRE(result.size() == 1);
+                REQUIRE(result.begin()->first == "v");
+                REQUIRE(result.begin()->second == 0);
             }
         }
     }
@@ -101,38 +73,40 @@ SCENARIO("Testing basic conditional effect EVMDD creation", "[CE_evmdd]") {
         ConditionalEffect effect =
             ConditionalEffect(Polynomial(e).getExpression(), "z", 0);
         ConditionalEffects effects = ConditionalEffects({effect});
-        Domains d = {{"x", 2}, {"y", 2}, {"z", 2}};
-        Ordering o = {{"z", 1}, {"y", 2}, {"x", 3}};
+        WHEN("We create the EVMDD") {
+            Domains d = {{"x", 2}, {"y", 2}, {"z", 2}};
+            Ordering o = {{"z", 1}, {"y", 2}, {"x", 3}};
 
-        Evmdd<VariableAssignmentExpression> evmdd = effects.create_evmdd(d, o);
-        // evmdd.print(std::cout);
-        THEN("EVMDD should be") {
-            std::stringstream expected;
-            expected << "input value: {} nodes:" << std::endl;
-            expected << "ID: 3(x)" << std::endl;
-            expected << "  w[0]: {}" << std::endl
-                     << "    ID: 1(y)" << std::endl;
-            expected << "      w[0]: {}" << std::endl
-                     << "        ID: 0( )" << std::endl;
-            expected << "      w[1]: { z=0}" << std::endl
-                     << "        ID: 0( )" << std::endl;
+            Evmdd<Facts, Union> evmdd = effects.create_evmdd(d, o);
+            THEN("EVMDD should be") {
+                std::stringstream expected;
+                expected << "input value: {} nodes:" << std::endl;
+                expected << "ID: 3(x)" << std::endl;
+                expected << "  w[0]: {}" << std::endl
+                         << "    ID: 1(y)" << std::endl;
+                expected << "      w[0]: {}" << std::endl
+                         << "        ID: 0( )" << std::endl;
+                expected << "      w[1]: {z=0}" << std::endl
+                         << "        ID: 0( )" << std::endl;
 
-            expected << "  w[1]: { z=0}" << std::endl
-                     << "    ID: 2(y)" << std::endl;
-            expected << "      w[0]: {}" << std::endl
-                     << "        ID: 0( )" << std::endl;
-            expected << "      w[1]: {}" << std::endl
-                     << "        ID: 0( )" << std::endl;
-            std::stringstream ss;
-            evmdd.print(ss);
-
-            REQUIRE(ss.str() == expected.str());
+                expected << "  w[1]: {z=0}" << std::endl
+                         << "    ID: 2(y)" << std::endl;
+                expected << "      w[0]: {}" << std::endl
+                         << "        ID: 0( )" << std::endl;
+                expected << "      w[1]: {}" << std::endl
+                         << "        ID: 0( )" << std::endl;
+                std::stringstream ss;
+                evmdd.print(ss);
+                REQUIRE(ss.str() == expected.str());
+            }
         }
     }
 
     GIVEN(
-        "The conditional effect:[var5==0 && var6==0 ]->var5==1) & ([var5==0 && "
-        "var6==0 ]->var6==1) & ([var5==0 && var6==1 ]->var5==1) & ([var5==0 && "
+        "The conditional effect:[var5==0 && var6==0 ]->var5==1) & "
+        "([var5==0 && "
+        "var6==0 ]->var6==1) & ([var5==0 && var6==1 ]->var5==1) & "
+        "([var5==0 && "
         "var6==1 ]->var6==2)") {
         std::string e =
             "([[var5==0]&&[var6==0]]->var5==1) & "
@@ -146,9 +120,11 @@ SCENARIO("Testing basic conditional effect EVMDD creation", "[CE_evmdd]") {
         THEN("Result should have 4 effects") {
             REQUIRE(effects.getEffects().size() == 4);
         }
-        Evmdd<VariableAssignmentExpression> evmdd = effects.create_evmdd(d, o);
+        Evmdd<Facts, Union> evmdd = effects.create_evmdd(d, o);
         THEN("EVMDD should be: ") {
             evmdd.print(std::cout);
+            // TODO implement test
+            REQUIRE(false);
         }
     }
 }
