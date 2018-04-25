f4575061cc7b59861aae9e1589a6a90f2df4dfa1
etavas <danerdeac@gmail.com>
2018-04-25 22:53:43 +0200

    building but non-proper code upload for review

--- src/evmdd/operations/logic_and.h
+++ src/evmdd/operations/logic_and.h
@@ -3,17 +3,44 @@
 
 #include <functional>
 
-template <class T>
+template <typename T> 
 struct logic_and {
     T operator()(const T& lhs, const T& rhs) const {
         if(!((lhs==0 || lhs==1) && (rhs==0 || rhs==1))){
             throw std::logic_error("Non-boolean operands for logic_and");
         }
-        assert((lhs==0 || lhs==1));
-        assert((rhs==0 || rhs==1));
-        auto result = std::logical_and<T>()(lhs, rhs);
-        return result;
     }
 };
+
+template <>
+struct logic_and <int> { 
+    int operator()(const int& lhs, const int& rhs) const {
+    assert((lhs==0 || lhs==1));
+    assert((rhs==0 || rhs==1));
+    auto result = std::logical_and<int>()(lhs, rhs);
+    return result;
+    }
+};
+
+template<>
+struct logic_and <double> { 
+    double operator()(const double& lhs, const double& rhs) const {
+    assert((lhs==0 || lhs==1));
+    assert((rhs==0 || rhs==1));
+    auto result = std::logical_and<double>()(lhs, rhs);
+    return result;
+    }
+};
+
+template<>
+struct logic_and <float> { 
+    float operator()(const float& lhs, const float& rhs) const {
+    assert((lhs==0 || lhs==1));
+    assert((rhs==0 || rhs==1));
+    auto result = std::logical_and<>()(lhs, rhs);
+    return result;
+    }
+};
+
 
 #endif /* LOGIC_AND_H */
