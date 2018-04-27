dac36513449ec42155f96164376d661668bb03d1
etavas <danerdeac@gmail.com>
2018-04-27 14:59:36 +0200

    Trial with #ifdef DEBUG

--- src/evmdd/operations/logic_and.h
+++ src/evmdd/operations/logic_and.h
@@ -3,20 +3,38 @@
 
 #include <functional>
 
+/**
+ * Template class logic_and throws exception if parameters are 
+ * none of the types int, float or double.
+ * */
 template <typename T> 
 struct logic_and {
-    T operator()(const T& lhs, const T& rhs) const {
-        if(!((lhs==0 || lhs==1) && (rhs==0 || rhs==1))){
-            throw std::logic_error("Non-boolean operands for logic_and");
-        }
+    T operator()(const T&, const T&) const {
+        throw std::logic_error("Non-boolean operands for logic_and");
     }
 };
+
+/**
+ * Checks if the operands are suitable for logic_and operation
+ * */
+#ifdef DEBUG
+template<typename T> 
+void domain_check(const T& lhs, const T& rhs){ 
+    assert((lhs==0 || lhs==1));
+    assert((rhs==0 || rhs==1));
+    if(!((lhs==0||lhs==1)&&(rhs==0||rhs==1))) {
+        throw std::logic_error("Domain size >2 for logic_and");
+    }
+}
+#endif
+
 
 template <>
 struct logic_and <int> { 
     int operator()(const int& lhs, const int& rhs) const {
-    assert((lhs==0 || lhs==1));
-    assert((rhs==0 || rhs==1));
+    #ifdef DEBUG
+    domain_check(lhs,rhs);
+    #endif
     auto result = std::logical_and<int>()(lhs, rhs);
     return result;
     }
@@ -25,8 +43,9 @@ struct logic_and <int> {
 template<>
 struct logic_and <double> { 
     double operator()(const double& lhs, const double& rhs) const {
-    assert((lhs==0 || lhs==1));
-    assert((rhs==0 || rhs==1));
+    #ifdef DEBUG
+    domain_check(lhs,rhs);
+    #endif
     auto result = std::logical_and<double>()(lhs, rhs);
     return result;
     }
@@ -35,8 +54,9 @@ struct logic_and <double> {
 template<>
 struct logic_and <float> { 
     float operator()(const float& lhs, const float& rhs) const {
-    assert((lhs==0 || lhs==1));
-    assert((rhs==0 || rhs==1));
+    #ifdef DEBUG
+    domain_check(lhs,rhs);
+    #endif
     auto result = std::logical_and<>()(lhs, rhs);
     return result;
     }
