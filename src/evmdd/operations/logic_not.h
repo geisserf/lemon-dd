6d357dee244ca75562b5c5355ac8fc4987143090
etavas <danerdeac@gmail.com>
2018-04-28 10:56:23 +0200

    Implemented domain size check for logical operators

--- src/evmdd/operations/logic_not.h
+++ src/evmdd/operations/logic_not.h
@@ -3,18 +3,59 @@
 
 #include <functional>
 
-template <class T>
-struct logic_not {
-    // Logical not operation disregards rhs and returns !lhs.
-    // This is a hack to support unary evmdd operations via apply
-    T operator()(const T& lhs, const T& /*rhs*/) const {
-        if(!(lhs==0 || lhs==1)) {
-            throw std::logic_error("Non-boolean operand for logical_not");
-        }
-        assert((lhs==0 || lhs==1));
-        auto result = std::logical_not<T>()(lhs);
-        return result;
-    }
+/**
+ * Logical not operation disregards rhs and returns !lhs.
+ * This is a hack to support unary evmdd operations via apply
+ */
+
+// Template class logic_or throws exception if parameters are
+// none of the types int, float or double.
+template <class T> struct logic_not {
+  T operator()(const T &, const T &) const {
+    throw std::logic_error("Non-boolean operand for logical_not");
+  }
 };
 
+// Checks if the operand lhs is binary
+#ifndef DEBUG
+template <class T> void domain_check_not(const T &lhs) {
+  if (!(lhs == 0 || lhs == 1)) {
+    throw std::logic_error("Domain size >2 for logic_not");
+  }
+  assert((lhs == 0 || lhs == 1));
+}
+#endif
+
+// Specialization for type int
+template <> struct logic_not<int> {
+  int operator()(const int &lhs, const int & /*rhs*/) const {
+#ifndef DEBUG
+    domain_check_not(lhs);
+#endif
+    auto result = std::logical_not<int>()(lhs);
+    return result;
+  }
+};
+
+// Specialization for type double
+template <> struct logic_not<double> {
+  double operator()(const double &lhs, const double & /*rhs*/) const {
+#ifndef DEBUG
+    domain_check_not(lhs);
+#endif
+    auto result = std::logical_not<double>()(lhs);
+    return result;
+  }
+};
+
+// Specialization for type float
+template <> struct logic_not<float> {
+  float operator()(const float &lhs, const float & /*rhs*/) const {
+#ifndef DEBUG
+    domain_check_not(lhs);
+#endif
+    auto result = std::logical_not<float>()(lhs);
+    return result;
+  }
+};
 #endif /* LOGIC_NOT_H */
--- src/evmdd/operations/logic_or.h
+++ src/evmdd/operations/logic_or.h
@@ -3,17 +3,56 @@
 
 #include <functional>
 
-template <class T>
-struct logic_or {
-    T operator()(const T& lhs, const T& rhs) const {
-        if(!((lhs==0 || lhs==1) && (rhs==0 || rhs==1))){
-            throw std::logic_error("Non-boolean operand for logical_or");
-        }
-        assert((lhs==0 || lhs==1));
-        assert((rhs==0 || rhs==1));
-        auto result = std::logical_or<T>()(lhs, rhs);
-        return result;
-    }
+// Template class logic_or throws exception if parameters are
+// none of the types int, float or double.
+template <class T> struct logic_or {
+  T operator()(const T &, const T &) const {
+    throw std::logic_error("Non-boolean operand for logical_or");
+  }
+};
+
+// Checks if the operands are binary
+#ifndef DEBUG
+template <class T> void domain_check_or(const T &lhs, const T &rhs) {
+  if (!((lhs == 0 || lhs == 1) && (rhs == 0 || rhs == 1))) {
+    throw std::logic_error("Domain size >2 for logic_or");
+  }
+  assert((lhs == 0 || lhs == 1));
+  assert((rhs == 0 || rhs == 1));
+}
+#endif
+
+// Specialization for type int
+template <> struct logic_or<int> {
+  int operator()(const int &lhs, const int &rhs) const {
+#ifndef DEBUG
+    domain_check_or(lhs, rhs);
+#endif
+    auto result = std::logical_or<int>()(lhs, rhs);
+    return result;
+  }
+};
+
+// Specialization for type double
+template <> struct logic_or<double> {
+  double operator()(const double &lhs, const double &rhs) const {
+#ifndef DEBUG
+    domain_check_or(lhs, rhs);
+#endif
+    auto result = std::logical_or<double>()(lhs, rhs);
+    return result;
+  }
+};
+
+// Specialization for type float
+template <> struct logic_or<float> {
+  float operator()(const float &lhs, const float &rhs) const {
+#ifndef DEBUG
+    domain_check_or(lhs, rhs);
+#endif
+    auto result = std::logical_or<float>()(lhs, rhs);
+    return result;
+  }
 };
 
 #endif /* LOGIC_NOT_H */
