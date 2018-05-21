505ad0db74f84cdd851ba286f8c3f35d0cff9efa
gt42 <danerdeac@gmail.com>
2018-05-21 20:23:41 +0200

    Added unit tests for partial ordering

--- src/test/evmdd/numeric/construction_tests_arithmetic.cpp
+++ src/test/evmdd/numeric/construction_tests_arithmetic.cpp
@@ -8,7 +8,7 @@ using std::endl;
 
 // Testing construction for expressions without logical propositions
 SCENARIO("Testing numeric EVMDD for construction on arithmetic input",
-         "[evmdd][construction]") {
+         "[evmdd][construction][arithmetic]") {
     GIVEN("Term 5") {
         Polynomial p = Polynomial("5");
         WHEN("We create the evmdd") {
@@ -96,7 +96,7 @@ SCENARIO("Testing numeric EVMDD for construction on arithmetic input",
     GIVEN("Term (x*y)+z with D(x)=D(y)=0,1 and D(z)=0,1,2") {
         Domains d = {{"x", 2}, {"y", 2}, {"z", 3}};
         Polynomial p = Polynomial("(x*y)+z");
-        WHEN("We create the evmdd in order x>y>z") {
+        WHEN("We create the evmdd in order x over y over z") {
             Ordering o = {"z", "y", "x"};
             auto evmdd = p.create_evmdd<int>(d, o);
             THEN("evmdd has the expected structure") {
@@ -111,8 +111,110 @@ SCENARIO("Testing numeric EVMDD for construction on arithmetic input",
                 REQUIRE(result.str() == expected.str());
             }
         }
-        WHEN("We create the evmdd in order x>z>y") {
+        WHEN("We create the evmdd in order x over z over y") {
             Ordering o = {"y", "z", "x"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "x 0 0" << endl;
+                expected << "z 0 1 2" << endl;
+                expected << "z 0 1 2" << endl;
+                expected << "y 0 0" << endl;
+                expected << "y 0 1" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+    }
+}
+
+SCENARIO(
+    "Testing numeric EVMDDs for construction with partial ordering on "
+    "arithmetic input",
+    "[evmdd][construction][arithmetic][partial]") {
+    GIVEN("Term (x*y)+z with D(x)=D(y)=0,1 and D(z)=0,1,2") {
+        Domains d = {{"x", 2}, {"y", 2}, {"z", 3}};
+        Polynomial p = Polynomial("(x*y)+z");
+        WHEN("We create the evmdd in order x over y") {
+            Ordering o = {"y", "x"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "z 0 1 2" << endl;
+                expected << "x 0 0" << endl;
+                expected << "y 0 0" << endl;
+                expected << "y 0 1" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+        WHEN("We create the evmdd in order y over x") {
+            Ordering o = {"x", "y"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "z 0 1 2" << endl;
+                expected << "y 0 0" << endl;
+                expected << "x 0 0" << endl;
+                expected << "x 0 1" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+        WHEN("We create the evmdd in order x over z") {
+            Ordering o = {"z", "x"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "y 0 0" << endl;
+                expected << "x 0 0" << endl;
+                expected << "x 0 1" << endl;
+                expected << "z 0 1 2" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+        WHEN("We create the evmdd in order z over x") {
+            Ordering o = {"x", "z"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "y 0 0" << endl;
+                expected << "z 0 1 2" << endl;
+                expected << "z 0 1 2" << endl;
+                expected << "x 0 0" << endl;
+                expected << "x 0 1" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+        WHEN("We create the evmdd in order y over z") {
+            Ordering o = {"z", "y"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "x 0 0" << endl;
+                expected << "y 0 0" << endl;
+                expected << "y 0 1" << endl;
+                expected << "z 0 1 2" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+        WHEN("We create the evmdd in order z over y") {
+            Ordering o = {"y", "z"};
             auto evmdd = p.create_evmdd<int>(d, o);
             THEN("evmdd has the expected structure") {
                 std::stringstream result;
--- src/test/evmdd/numeric/construction_tests_iversion.cpp
+++ src/test/evmdd/numeric/construction_tests_iversion.cpp
@@ -174,7 +174,7 @@ SCENARIO("Testing numeric EVMDDs for construction on iverson based input",
         Polynomial p = Polynomial("[a&&[!b]]*c+5");
         WHEN("We create the evmdd") {
             Domains d = {{"a", 2}, {"b", 2}, {"c", 2}};
-            Ordering o = {"c","b","a"};
+            Ordering o = {"c", "b", "a"};
             auto evmdd = p.create_evmdd<int>(d, o);
             THEN("evmdd has the expected structure") {
                 std::stringstream result;
@@ -196,14 +196,12 @@ SCENARIO("Testing numeric EVMDDs for construction on iverson based input",
      *  Tests for logical expressions on non-bool. variable valuations
      *
      *****************************************************************/
-    //  TODO : Edit source code to throw exceptions in this case
-    //  IMPORTANT : == is valid for non-bool. values
 
     GIVEN("Term [a==b] with domain Da=Db={0,1,2}") {
         Polynomial p = Polynomial("[a==b]");
         WHEN("We create the evmdd") {
             Domains d = {{"a", 3}, {"b", 3}};
-            Ordering o = {"b","a"};
+            Ordering o = {"b", "a"};
             auto evmdd = p.create_evmdd<int>(d, o);
             THEN("evmdd has the expected structure") {
                 std::stringstream result;
@@ -219,9 +217,6 @@ SCENARIO("Testing numeric EVMDDs for construction on iverson based input",
         }
     }
 
-    // From this point, tests do not run correctly, because the source code
-    // is not edited accordingly yet. @etavas
-
     GIVEN("Term [!a] with domain 0,1,2") {
         Polynomial p = Polynomial("[!a]");
         Domains d = {{"a", 3}};
@@ -235,7 +230,7 @@ SCENARIO("Testing numeric EVMDDs for construction on iverson based input",
     GIVEN("Term [a && b] with domains Da ={0,1} Db = {0,1,2}") {
         Polynomial p = Polynomial("[a&&b]");
         Domains d = {{"a", 2}, {"b", 3}};
-        Ordering o = {"b","a"};
+        Ordering o = {"b", "a"};
         WHEN("We create the evmdd") {
             THEN("We get an error") {
                 REQUIRE_THROWS_AS(p.create_evmdd<int>(d, o), std::logic_error);
@@ -245,7 +240,7 @@ SCENARIO("Testing numeric EVMDDs for construction on iverson based input",
     GIVEN("Term [a || b] with domains Da = Db = {0,1,2}") {
         Polynomial p = Polynomial("[a || b]");
         Domains d = {{"a", 3}, {"b", 3}};
-        Ordering o = {"b","a"};
+        Ordering o = {"b", "a"};
         WHEN("We create the evmdd") {
             THEN("We get an error") {
                 REQUIRE_THROWS_AS(p.create_evmdd<int>(d, o), std::logic_error);
@@ -268,7 +263,7 @@ SCENARIO("Testing numeric EVMDDs for construction on iverson based input",
     GIVEN("Term [!a]+b with domain Da=Db={0,1,2}") {
         Polynomial p = Polynomial("[!a]+b");
         Domains d = {{"a", 3}, {"b", 3}};
-        Ordering o = {"b","a"};
+        Ordering o = {"b", "a"};
         WHEN("We create the evmdd") {
             THEN("We get an error") {
                 REQUIRE_THROWS_AS(p.create_evmdd<int>(d, o), std::logic_error);
@@ -280,7 +275,7 @@ SCENARIO("Testing numeric EVMDDs for construction on iverson based input",
         Polynomial p = Polynomial("[!a]+b");
         WHEN("We create the evmdd") {
             Domains d = {{"a", 2}, {"b", 3}};
-            Ordering o = {"b","a"};
+            Ordering o = {"b", "a"};
             auto evmdd = p.create_evmdd<int>(d, o);
             THEN("evmdd has the expected structure") {
                 std::stringstream result;
@@ -289,6 +284,62 @@ SCENARIO("Testing numeric EVMDDs for construction on iverson based input",
                 expected << "input: 0" << endl;
                 expected << "a 1 0" << endl;
                 expected << "b 0 1 2" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+    }
+}
+
+SCENARIO(
+    "Testing numeric EVMDDs for construction with partial ordering on iverson "
+    "based input",
+    "[evmdd][construction][iversion][partial]") {
+    GIVEN("Term [a&&b]+c with domain 0,1") {
+        Domains d = {{"a", 2}, {"b", 2}, {"c", 2}};
+        Polynomial p = Polynomial("[a&&b]+c");
+        WHEN("We create the evmdd with ordering a over b") {
+            Ordering o = {"b", "a"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "c 0 1" << endl;
+                expected << "a 0 0" << endl;
+                expected << "b 0 0" << endl;
+                expected << "b 0 1" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+        WHEN("We create the evmdd with ordering b over a") {
+            Ordering o = {"a", "b"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "c 0 1" << endl;
+                expected << "b 0 0" << endl;
+                expected << "a 0 0" << endl;
+                expected << "a 0 1" << endl;
+                REQUIRE(result.str() == expected.str());
+            }
+        }
+        WHEN("We create the evmdd with ordering c over a") {
+            Ordering o = {"a", "c"};
+            auto evmdd = p.create_evmdd<int>(d, o);
+            THEN("evmdd has the expected structure") {
+                std::stringstream result;
+                evmdd.print(result);
+                std::stringstream expected;
+                expected << "input: 0" << endl;
+                expected << "b 0 0" << endl;
+                expected << "c 0 1" << endl;
+                expected << "c 0 1" << endl;
+                expected << "a 0 0" << endl;
+                expected << "a 0 1" << endl;
                 REQUIRE(result.str() == expected.str());
             }
         }
