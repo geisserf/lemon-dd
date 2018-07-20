d2559cd46a3716f68f089e17f060483928b33835
gt42 <danerdeac@gmail.com>
2018-07-20 14:31:21 +0200

    Define annihilator and identity

--- src/evmdd/operations/logic_and.h
+++ src/evmdd/operations/logic_and.h
@@ -5,54 +5,61 @@
 
 // Template class logic_and throws exception if parameters are
 // none of the types int, float or double.
-template <class T> struct logic_and {
-  T operator()(const T &, const T &) const {
-    throw std::logic_error("Non-boolean operands for logic_and");
-  }
+template <class T>
+struct logic_and {
+    T operator()(const T &, const T &) const {
+        static bool annihilator = false;
+        static bool identity = true;
+        throw std::logic_error("Non-boolean operands for logic_and");
+    }
 };
 
 // Checks if the operands are binary
 #ifndef DEBUG
-template <class T> void domain_check_and(const T &lhs, const T &rhs) {
-  if (!((lhs == 0 || lhs == 1) && (rhs == 0 || rhs == 1))) {
-    throw std::logic_error("Domain size >2 for logic_and");
-  }
-  assert((lhs == 0 || lhs == 1));
-  assert((rhs == 0 || rhs == 1));
+template <class T>
+void domain_check_and(const T &lhs, const T &rhs) {
+    if (!((lhs == 0 || lhs == 1) && (rhs == 0 || rhs == 1))) {
+        throw std::logic_error("Domain size >2 for logic_and");
+    }
+    assert((lhs == 0 || lhs == 1));
+    assert((rhs == 0 || rhs == 1));
 }
 #endif
 
 // Specialization for type int
-template <> struct logic_and<int> {
-  int operator()(const int &lhs, const int &rhs) const {
+template <>
+struct logic_and<int> {
+    int operator()(const int &lhs, const int &rhs) const {
 #ifndef DEBUG
-    domain_check_and(lhs, rhs);
+        domain_check_and(lhs, rhs);
 #endif
-    auto result = std::logical_and<int>()(lhs, rhs);
-    return result;
-  }
+        auto result = std::logical_and<int>()(lhs, rhs);
+        return result;
+    }
 };
 
 // Specialization for type double
-template <> struct logic_and<double> {
-  double operator()(const double &lhs, const double &rhs) const {
+template <>
+struct logic_and<double> {
+    double operator()(const double &lhs, const double &rhs) const {
 #ifndef DEBUG
-    domain_check_and(lhs, rhs);
+        domain_check_and(lhs, rhs);
 #endif
-    auto result = std::logical_and<double>()(lhs, rhs);
-    return result;
-  }
+        auto result = std::logical_and<double>()(lhs, rhs);
+        return result;
+    }
 };
 
 // Specialization for type float
-template <> struct logic_and<float> {
-  float operator()(const float &lhs, const float &rhs) const {
+template <>
+struct logic_and<float> {
+    float operator()(const float &lhs, const float &rhs) const {
 #ifndef DEBUG
-    domain_check_and(lhs, rhs);
+        domain_check_and(lhs, rhs);
 #endif
-    auto result = std::logical_and<float>()(lhs, rhs);
-    return result;
-  }
+        auto result = std::logical_and<float>()(lhs, rhs);
+        return result;
+    }
 };
 
 #endif /* LOGIC_AND_H */
--- src/evmdd/operations/logic_or.h
+++ src/evmdd/operations/logic_or.h
@@ -5,54 +5,61 @@
 
 // Template class logic_or throws exception if parameters are
 // none of the types int, float or double.
-template <class T> struct logic_or {
-  T operator()(const T &, const T &) const {
-    throw std::logic_error("Non-boolean operand for logical_or");
-  }
+template <class T>
+struct logic_or {
+    T operator()(const T &, const T &) const {
+        static bool annihilator = true;
+        static bool identity = false;
+        throw std::logic_error("Non-boolean operand for logical_or");
+    }
 };
 
 // Checks if the operands are binary
 #ifndef DEBUG
-template <class T> void domain_check_or(const T &lhs, const T &rhs) {
-  if (!((lhs == 0 || lhs == 1) && (rhs == 0 || rhs == 1))) {
-    throw std::logic_error("Domain size >2 for logic_or");
-  }
-  assert((lhs == 0 || lhs == 1));
-  assert((rhs == 0 || rhs == 1));
+template <class T>
+void domain_check_or(const T &lhs, const T &rhs) {
+    if (!((lhs == 0 || lhs == 1) && (rhs == 0 || rhs == 1))) {
+        throw std::logic_error("Domain size >2 for logic_or");
+    }
+    assert((lhs == 0 || lhs == 1));
+    assert((rhs == 0 || rhs == 1));
 }
 #endif
 
 // Specialization for type int
-template <> struct logic_or<int> {
-  int operator()(const int &lhs, const int &rhs) const {
+template <>
+struct logic_or<int> {
+    int operator()(const int &lhs, const int &rhs) const {
 #ifndef DEBUG
-    domain_check_or(lhs, rhs);
+        domain_check_or(lhs, rhs);
 #endif
-    auto result = std::logical_or<int>()(lhs, rhs);
-    return result;
-  }
+        auto result = std::logical_or<int>()(lhs, rhs);
+        return result;
+    }
 };
 
 // Specialization for type double
-template <> struct logic_or<double> {
-  double operator()(const double &lhs, const double &rhs) const {
+template <>
+struct logic_or<double> {
+    double operator()(const double &lhs, const double &rhs) const {
 #ifndef DEBUG
-    domain_check_or(lhs, rhs);
+        domain_check_or(lhs, rhs);
 #endif
-    auto result = std::logical_or<double>()(lhs, rhs);
-    return result;
-  }
+        auto result = std::logical_or<double>()(lhs, rhs);
+        return result;
+    }
 };
 
 // Specialization for type float
-template <> struct logic_or<float> {
-  float operator()(const float &lhs, const float &rhs) const {
+template <>
+struct logic_or<float> {
+    float operator()(const float &lhs, const float &rhs) const {
 #ifndef DEBUG
-    domain_check_or(lhs, rhs);
+        domain_check_or(lhs, rhs);
 #endif
-    auto result = std::logical_or<float>()(lhs, rhs);
-    return result;
-  }
+        auto result = std::logical_or<float>()(lhs, rhs);
+        return result;
+    }
 };
 
 #endif /* LOGIC_NOT_H */
