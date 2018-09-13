9008c7419217a8cdea0aa0fa6bef18c4e733b11f
gt42 <danerdeac@gmail.com>
2018-09-13 12:40:12 +0200

    Implemented header for equal_to

---
+++ src/evmdd/operations/equal_to.h
@@ -0,0 +1,22 @@
+#ifndef EQUAL_TO_H
+#define EQUAL_TO_H
+
+#include <functional>
+
+template <class T>
+struct equal_to {
+    static const T annihilator, identity;
+    T operator()(const T &lhs, const T &rhs) const {
+        auto result = std::equal_to<T>()(lhs, rhs);
+        return result;
+    }
+    constexpr bool has_identity() const {
+        return false;
+    }
+
+    constexpr bool has_annihilator() const {
+        return false;
+    }
+};
+
+#endif /* EQUAL_TO_H */
